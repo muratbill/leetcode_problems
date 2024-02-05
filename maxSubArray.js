a66f2b8d6ac2b944888d1a25217c415573f356c5
muratbill <43638859+muratbill@users.noreply.github.com>
2024-02-05 13:12:07 +0300

    Create maxSubArray.js

---
+++ maxSubArray.js
@@ -0,0 +1,10 @@
+var maxSubArray = function(nums) {
+    let max = nums[0];
+    let current = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        current = Math.max(nums[i], current + nums[i]);
+        max = Math.max(max, current);
+    }
+    return max;
+
+};
